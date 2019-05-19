d538138d0c7a191d6969b78fe0294760b1e11953
indianatoms <ttomek.koziak@gmail.com>
2019-05-19 14:20:10 +0200

    Making Transitions
    Kinda spaghetti code but it is working...
    Making transitions on Entity A form state 0 to state 1 - sending and receiving a.

--- Main.java
+++ Main.java
@@ -13,6 +13,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 
 public class Main {
@@ -20,15 +22,17 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         List<MyEntity> myEntityList = new ArrayList<MyEntity>();
-
+        int Anumber = 0;
+        int Bnumber = 0;
         int number = 0;
+        int counter = 0;
+        String queue = "";
         char a = 'a';
         int b = 0;
         char c = 'c';
         char d = 'd';
         int e = 0;
 
-        //      ArrayList<Entity> arrayList = new ArrayList<>();
 
         String[] tokens = new String[5];
 
@@ -51,8 +55,62 @@ public class Main {
 
         for(int i = 0; i < myEntityList.size(); i++) {
             myEntityList.get(i).PrintElements();
+            if(myEntityList.get(i).getEnitityLetter() == 'A')
+            {
+                Anumber++;
+            }
+            if(myEntityList.get(i).getEnitityLetter() == 'B')
+            {
+                Bnumber++;
+            }
         }
+        System.out.println(Anumber);
+        System.out.println(Bnumber);
 
+        int Acurrent = 0,Bcurrent = Anumber, RandomWalk;
+        List<Integer> APossibleTransitions = new ArrayList<Integer>();
+        while (myEntityList.get(Acurrent).getDestiantion() != -1 && myEntityList.get(Bcurrent).getDestiantion() != -1)
+        {
+            for(int i = 0; i < myEntityList.size(); i++) {
+                if (myEntityList.get(i).getEnitityLetter() == 'A' && myEntityList.get(i).getStateNumber() == Acurrent) {
+                    APossibleTransitions.add(myEntityList.get(i).getDestiantion());
+                }
+            }
+            do {
+                RandomWalk = ThreadLocalRandom.current().nextInt(0, APossibleTransitions.size() + 1);
+            }while (RandomWalk == Acurrent);
+            System.out.println("Entity A");
+            System.out.println("Transition form " + Acurrent + " to " + RandomWalk + " state");
+            for(int i = 0; i < myEntityList.size(); i++) {
+                if (myEntityList.get(i).getEnitityLetter() == 'A' && myEntityList.get(i).getStateNumber() == Acurrent && myEntityList.get(i).getDestiantion() == RandomWalk)
+                {
+
+                    if(myEntityList.get(i).getTransmision() == '-')
+                    {
+                        System.out.println("Sanding " + myEntityList.get(i).getMessage() + " to Queue");
+                        queue = queue + myEntityList.get(i).getMessage();
+                    }
+                    else if (myEntityList.get(i).getTransmision() == '+')
+                    {
+                        System.out.println("Receiving " + myEntityList.get(i).getMessage() + "from Queue");
+                        queue = queue + "-" + myEntityList.get(i).getMessage();
+                    }
+                    else
+                    {
+                        System.out.println("No transtions made");
+                    }
+                    break;
+                }
+            }
+            Acurrent = RandomWalk;
+            System.out.println("Queue looks like this: " + queue);
+            APossibleTransitions.clear();
+            counter++;
+            if(counter == 10)
+            {
+                break;
+            }
+        }
     }
 
 
@@ -98,13 +156,14 @@ public class Main {
 
         public void PrintElements()
         {
-            System.out.println("here");
+            System.out.println("Next Element");
             System.out.println(enitityLetter);
             System.out.println(stateNumber);
             System.out.println(transmision);
             System.out.println(message);
             System.out.println(destiantion);
         }
+
     }
 }
 
