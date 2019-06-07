d2e2c5875ff00d4c74e7db1de523fe193720acdb
indianatoms <ttomek.koziak@gmail.com>
2019-06-07 18:49:58 +0200

    New Aproach

--- Main.java
+++ Main.java
@@ -1,13 +1,9 @@
 package com.company;
 
 
-//zczytanie z inputu +
-//chodzenie po stanach +
-//wykrywanie blockowan w A i B
+
 
 //TODO:
-//modularnosc nazw
-//rozbudowanie do wiekszej ilosci entities
 //
 
 
@@ -15,20 +11,15 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.ThreadLocalRandom;
 
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        List<MyEntity> myEntityList = new ArrayList<MyEntity>();
-        int Anumber = 0;
-        int Bnumber = 0;
+        EntityList entityList = new EntityList();
         int number = 0;
         int counter = 0;
-        String queue = "";
         char a = 'a';
         int b = 0;
         char c = 'c';
@@ -42,126 +33,48 @@ public class Main {
 
         String line;
         while ((line = in.readLine()) != null) {
-            number++;
             System.out.println(line);
-            tokens = line.split(",|;");
-            a = tokens[0].charAt(0);
-            b = Integer.parseInt(tokens[1]);
-            c = tokens[2].charAt(0);
-            d = tokens[3].charAt(0);
-            e = Integer.parseInt(tokens[4]);
+            if(number == 0)
+            {
+                tokens = line.split(",|;");
+                int howMany = Integer.parseInt(tokens[0]);
+                for(int i = 1; i<howMany + 1; i++)
+                {
+                    Entity entity = new Entity(tokens[i].charAt(0),false,0);
+                    entityList.addEntity(entity);
+                }
+                entityList.printEntities();
+            }
+            else {
+                tokens = line.split(",|;");
+                a = tokens[0].charAt(0);
+                b = Integer.parseInt(tokens[1]);
+                c = tokens[2].charAt(0);
+                d = tokens[3].charAt(0);
+                e = Integer.parseInt(tokens[4]);
+                State state = new State(b,c,d,e);
+                entityList.getEntity(a).addState(state);
+            }
+            number++;
 
-            myEntityList.add(new MyEntity(a,b,c,d,e));
         }
         System.out.println(number);
+        //entityList.printAllStates();
+        //entityList.printEntitiyStates('A');
+        //entityList.printEntitiyStates('B');
+        //entityList.printEntitiyStates('C');
 
-        for(int i = 0; i < myEntityList.size(); i++) {
-            if(myEntityList.get(i).getEnitityLetter() == 'A')
-            {
-                Anumber++;
-            }
-            if(myEntityList.get(i).getEnitityLetter() == 'B')
-            {
-                Bnumber++;
-            }
-        }
-        System.out.println(Anumber);
-        System.out.println(Bnumber);
+        //Make Transitions
 
-        int Acurrent = 0,Bcurrent = Anumber, RandomWalk;
-        boolean Ablocked = false;
-        boolean Bblocked = false;
-        List<Integer> APossibleTransitions = new ArrayList<Integer>();
-        List<Integer> BPossibleTransitions = new ArrayList<Integer>();
-        
-        //warunek petli jest bez sensu
-        while (myEntityList.get(Acurrent).getDestiantion() != -1 && myEntityList.get(Bcurrent).getDestiantion() != -1)
+        while (counter != 50)
         {
-            for(int i = 0; i < myEntityList.size(); i++) {
-                if (myEntityList.get(i).getEnitityLetter() == 'A' && myEntityList.get(i).getStateNumber() == Acurrent) {
-                    APossibleTransitions.add(myEntityList.get(i).getDestiantion());
-                }
-            }
 
-            if(APossibleTransitions.size() == 0)
+            if(entityList.CheckIfAllBlocked() && entityList.CheckIfAllQueuesEmpty())
             {
-                System.out.println("A Has no other trasitions");
-                Ablocked = true;
-            }else {
-                //wychodzi na to ze mozna wylosowac samego siebie i nie ma z tym problemu
-                do {
-                    RandomWalk = ThreadLocalRandom.current().nextInt(0, APossibleTransitions.size());
-                } while (APossibleTransitions.get(RandomWalk) == Acurrent);
+                System.out.println("DEADLOCK OCCURED");
 
-                System.out.println("Entity A");
-                System.out.println("=====================================================================");
-                System.out.println("Transition form " + Acurrent + " to " + APossibleTransitions.get(RandomWalk) + " state");
-                for (int i = 0; i < myEntityList.size(); i++) {
-                    if (myEntityList.get(i).getEnitityLetter() == 'A' && myEntityList.get(i).getStateNumber() == Acurrent && myEntityList.get(i).getDestiantion() == APossibleTransitions.get(RandomWalk)) {
-
-                        if (myEntityList.get(i).getTransmision() == '-') {
-                            System.out.println("Sanding " + myEntityList.get(i).getMessage() + " to Queue");
-                            queue = queue + myEntityList.get(i).getMessage();
-                        } else if (myEntityList.get(i).getTransmision() == '+') {
-                            System.out.println("Receiving " + myEntityList.get(i).getMessage() + " from Queue");
-                            queue = removeFirst(myEntityList.get(i).getMessage(), queue);
-                        } else {
-                            System.out.println("No transtions made");
-                        }
-                        break;
-                    }
-                }
-                Acurrent = APossibleTransitions.get(RandomWalk);
-                System.out.println("Queue looks like this: " + queue);
-                APossibleTransitions.clear();
-            }
-            for(int i = 0; i < myEntityList.size(); i++) {
-                if (myEntityList.get(i).getEnitityLetter() == 'B' && myEntityList.get(i).getStateNumber() == Bcurrent) {
-                    BPossibleTransitions.add(myEntityList.get(i).getDestiantion());
-                }
-            }
-
-            if(BPossibleTransitions.size() == 0)
-            {
-                System.out.println("B Has no other trasitions");
-                Bblocked = true;
-            }else {
-                do {
-                    RandomWalk = ThreadLocalRandom.current().nextInt(0, BPossibleTransitions.size());
-                } while (BPossibleTransitions.get(RandomWalk) == Bcurrent);
-
-                System.out.println("=====================================================================");
-                System.out.println("Entity B");
-                System.out.println("Transition form " + Bcurrent + " to " + BPossibleTransitions.get(RandomWalk) + " state");
-                for (int i = 0; i < myEntityList.size(); i++) {
-                    if (myEntityList.get(i).getEnitityLetter() == 'B' && myEntityList.get(i).getStateNumber() == Bcurrent && myEntityList.get(i).getDestiantion() == BPossibleTransitions.get(RandomWalk)) {
-
-                        if (myEntityList.get(i).getTransmision() == '-') {
-                            System.out.println("Sanding " + myEntityList.get(i).getMessage() + " to Queue");
-                            queue = queue + myEntityList.get(i).getMessage();
-                        } else if (myEntityList.get(i).getTransmision() == '+') {
-                            System.out.println("Receiving " + myEntityList.get(i).getMessage() + " from Queue");
-                            queue = removeFirst(myEntityList.get(i).getMessage(), queue);
-                        } else {
-                            System.out.println("No transtions made");
-                        }
-                        break;
-                    }
-                }
-                Bcurrent = BPossibleTransitions.get(RandomWalk);
-                System.out.println("Queue looks like this: " + queue);
-                BPossibleTransitions.clear();
             }
             counter++;
-            if(counter == 4)
-            {
-                break;
-            }
-            if(Ablocked && queue.isEmpty() && Bblocked)
-            {
-                System.out.println("DeadLock occured");
-                break;
-            }
         }
     }
 
@@ -175,51 +88,156 @@ public class Main {
     }
 
 
+    public static class EntityList{
+        private ArrayList<Entity> entityList = new ArrayList<>();
 
-
-    public static class MyEntity {
-        private char enitityLetter;
-        private int stateNumber;
-        private char transmision;
-        private char message;
-        private int destiantion;
-
-        public char getEnitityLetter() {
-            return enitityLetter;
+        public void addEntity(Entity entity){
+            entityList.add(entity);
         }
+
+        public void printEntities() {
+            for (int i = 0; i< entityList.size(); i++)
+            {
+                System.out.println((entityList.get(i).name));
+            }
+        }
+
+        public Entity getEntity(char name){
+            for (int i = 0; i< entityList.size(); i++)
+            {
+                if(entityList.get(i).name == name)
+                {
+                    return entityList.get(i);
+                }
+            }
+            return null;
+        }
+
+        public boolean CheckIfAllBlocked()
+        {
+            for (int i = 0; i< entityList.size(); i++)
+            {
+                if (!entityList.get(i).isBlocked)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public boolean CheckIfAllQueuesEmpty()
+        {
+            for (int i = 0; i< entityList.size(); i++)
+            {
+                if (!entityList.get(i).isQueueEmpty())
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public  void printAllStates()
+        {
+            for (int i = 0; i< entityList.size(); i++)
+            {
+                entityList.get(i).printStates();
+            }
+        }
+
+        public  void printEntitiyStates(char name)
+        {
+            for (int i = 0; i< entityList.size(); i++)
+            {
+                if(entityList.get(i).getName() == name)
+                    entityList.get(i).printStates();
+            }
+        }
+
+        public void printCurrentStates()
+        {
+            for (int i = 0; i< entityList.size(); i++)
+            {
+                System.out.println(entityList.get(i).getName() + " is in state " + entityList.get(i).getCurrentState());
+            }
+        }
+
+    }
+
+    public static class Entity {
+        private char name;
+        private ArrayList<State> stateList = new ArrayList<>();
+        private String queue;
+        private int currentState;
+        private boolean isBlocked;
+
+        public Entity(char name, boolean isBlocked,int currentState) {
+            this.name = name;
+            this.isBlocked = isBlocked;
+            this.queue = "";
+            this.currentState = currentState;
+        }
+
+        public void addState(State state){
+            stateList.add(state);
+        }
+
+        public void printStates(){
+            for (int i = 0; i< stateList.size(); i++)
+            {
+                stateList.get(i).PrintState();
+            }
+        }
+
+        public char getName() {
+            return name;
+        }
+
+        public int getCurrentState() {
+            return currentState;
+        }
+
+        public boolean isQueueEmpty()  {return this.queue.isEmpty();}
+    }
+
+
+    public static class State {
+        private int stateNumber;
+        private char transition;
+        private char message;
+        private int destination;
+
 
         public int getStateNumber() {
             return stateNumber;
         }
 
-        public char getTransmision() {
-            return transmision;
+        public char getTransition() {
+            return transition;
         }
 
         public char getMessage() {
             return message;
         }
 
-        public int getDestiantion() {
-            return destiantion;
+        public int getDestination() {
+            return destination;
         }
 
-        public MyEntity(char enitityLetter, int stateNumber, char transmision, char message, int destiantion) {
-            this.enitityLetter = enitityLetter;
+        public State(int stateNumber, char transition, char message, int destination) {
             this.stateNumber = stateNumber;
-            this.transmision = transmision;
+            this.transition = transition;
             this.message = message;
-            this.destiantion = destiantion;
+            this.destination = destination;
         }
 
-        public void PrintElements()
+        public void PrintState()
         {
             System.out.println("Next Element");
-            System.out.println(enitityLetter);
             System.out.println(stateNumber);
-            System.out.println(transmision);
+            System.out.println(transition);
             System.out.println(message);
-            System.out.println(destiantion);
+            System.out.println(destination);
         }
 
     }
