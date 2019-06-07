a897702e08a796cd404e9616b08ad47aeb84dfe2
indianatoms <ttomek.koziak@gmail.com>
2019-06-08 01:09:18 +0200

    ecopr jakkolwiek dziala

--- Main.java
+++ Main.java
@@ -1,16 +1,19 @@
 package com.company;
 
 
+//Jestem na etapie zczytywania z txt inputu do programu teraz bede to przetwarzac
 
+import org.w3c.dom.Node;
 
-//TODO:
-//
-
-
+import javax.swing.text.html.parser.Entity;
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 
 public class Main {
@@ -18,64 +21,69 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         EntityList entityList = new EntityList();
-        int number = 0;
         int counter = 0;
+        int number = 0;
         char a = 'a';
         int b = 0;
         char c = 'c';
         char d = 'd';
         int e = 0;
+        char f = 'f';
 
+        //      ArrayList<Entity> arrayList = new ArrayList<>();
 
-        String[] tokens = new String[5];
+        String[] tokens;
 
         BufferedReader in = new BufferedReader(new FileReader("test.txt"));
 
         String line;
         while ((line = in.readLine()) != null) {
+
             System.out.println(line);
+            tokens = line.split(",|;");
+
+
             if(number == 0)
             {
-                tokens = line.split(",|;");
+
                 int howMany = Integer.parseInt(tokens[0]);
                 for(int i = 1; i<howMany + 1; i++)
                 {
-                    Entity entity = new Entity(tokens[i].charAt(0),false,0);
+                    Entity entity = new Entity(tokens[i].charAt(0),false,1);
                     entityList.addEntity(entity);
                 }
                 entityList.printEntities();
             }
             else {
-                tokens = line.split(",|;");
                 a = tokens[0].charAt(0);
                 b = Integer.parseInt(tokens[1]);
                 c = tokens[2].charAt(0);
                 d = tokens[3].charAt(0);
                 e = Integer.parseInt(tokens[4]);
-                State state = new State(b,c,d,e);
+                f = tokens[5].charAt(0);
+
+                State state = new State(b,c,d,e,f);
                 entityList.getEntity(a).addState(state);
             }
             number++;
 
         }
         System.out.println(number);
-        //entityList.printAllStates();
-        //entityList.printEntitiyStates('A');
-        //entityList.printEntitiyStates('B');
-        //entityList.printEntitiyStates('C');
 
-        //Make Transitions
 
         while (counter != 50)
         {
-
-            if(entityList.CheckIfAllBlocked() && entityList.CheckIfAllQueuesEmpty())
+            entityList.RandomWalk();
+            if(entityList.CheckIfAllQueuesEmpty() && entityList.isBlocked)
             {
                 System.out.println("DEADLOCK OCCURED");
-
+                break;
             }
             counter++;
         }
+        //entityList.printEntitiyStates('A');
+        //entityList.printEntitiyStates('B');
+        //entityList.printEntitiyStates('C');
     }
 
 
@@ -88,8 +96,11 @@ public class Main {
     }
 
 
+
+
     public static class EntityList{
         private ArrayList<Entity> entityList = new ArrayList<>();
+        private boolean isBlocked = false;
 
         public void addEntity(Entity entity){
             entityList.add(entity);
@@ -162,6 +173,85 @@ public class Main {
             }
         }
 
+        public void RandomWalk()
+        {
+            ArrayList<PossibleMove> possibleMoves = new ArrayList<>();
+
+            for (int i = 0; i< entityList.size(); i++)
+            {
+                Entity entity = entityList.get(i);
+                for (int j = 0; j< entity.getStateList().size(); j++)
+                {
+                    State current = entity.getStateList().get(j);
+                    if(current.getStateNumber() == entity.currentState)
+                    {
+                        PossibleMove possibleMove = new PossibleMove(current.getStateNumber(),current.getDestination(),current.getReceiver(),entity.getName());
+                        possibleMoves.add(possibleMove);
+                    }
+                }
+            }
+            if(possibleMoves.size() == 0)
+            {
+                isBlocked = true;
+            }
+            System.out.println("Number rof possible moves " + possibleMoves.size());
+            int randomNum = ThreadLocalRandom.current().nextInt(0, possibleMoves.size());
+            System.out.println("Number randomly choosen is " + randomNum);
+            PossibleMove chosenMove = possibleMoves.get(randomNum);
+            makeTransition(chosenMove.getEntity(),chosenMove.getReciever(),chosenMove.getTarget());
+        }
+
+
+
+
+        public boolean makeTransition (char entitiy, char targetEntity, int destination)
+        {
+            for (int i = 0; i< entityList.size(); i++)
+            {
+                if(entityList.get(i).getName() == entitiy)
+                {
+                    Entity entity =  entityList.get(i);
+                    for (int j = 0; j< entity.getStateList().size(); j++)
+                    {
+                        if(entity.getStateList().get(j).getStateNumber() == entity.getCurrentState())
+                        {
+                            if(entity.getStateList().get(j).getDestination() == destination && entity.getStateList().get(j).getReceiver() == targetEntity)
+                            {
+                                State state = entity.getStateList().get(j);
+                                if(state.getTransition() == '+')
+                                {
+                                    entity.AddElementToQueue(state.getMessage());
+                                    System.out.println("Message Send");
+                                    entity.setCurrentState(state.getDestination());
+                                    return true;
+                                }
+                                else if (state.getTransition() == '+')
+                                {
+                                    if(entity.getQueue() == removeFirst(state.getMessage(),entity.getQueue()))
+                                    {
+                                        System.out.println("No Such Element in the Queue");
+                                        return false;
+                                    }
+                                    else {
+                                        entity.RemoveElementFromQueue(state.getMessage());
+                                        return true;
+                                    }
+                                }
+                                else if (state.getTransition() == 't')
+                                {
+                                    System.out.println("Empty Transitions Occured");
+                                    entity.setCurrentState(state.getDestination());
+                                    return true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+
     }
 
     public static class Entity {
@@ -171,10 +261,28 @@ public class Main {
         private int currentState;
         private boolean isBlocked;
 
-        public Entity(char name, boolean isBlocked,int currentState) {
+        public String getQueue() {
+            return queue;
+        }
+
+        public Entity(char name, boolean isBlocked, int currentState) {
             this.name = name;
             this.isBlocked = isBlocked;
             this.queue = "";
+            this.currentState = currentState;
+        }
+
+        public void AddElementToQueue(char add)
+        {
+            queue += add;
+        }
+
+        public void RemoveElementFromQueue(char sign)
+        {
+            queue = removeFirst(sign,queue);
+        }
+
+        public void setCurrentState(int currentState) {
             this.currentState = currentState;
         }
 
@@ -198,6 +306,10 @@ public class Main {
         }
 
         public boolean isQueueEmpty()  {return this.queue.isEmpty();}
+
+        public ArrayList<State> getStateList() {
+            return stateList;
+        }
     }
 
 
@@ -205,8 +317,12 @@ public class Main {
         private int stateNumber;
         private char transition;
         private char message;
+        private char receiver;
         private int destination;
 
+        public char getReceiver() {
+            return receiver;
+        }
 
         public int getStateNumber() {
             return stateNumber;
@@ -224,11 +340,12 @@ public class Main {
             return destination;
         }
 
-        public State(int stateNumber, char transition, char message, int destination) {
+        public State(int stateNumber, char transition, char message, int destination, char receiver) {
             this.stateNumber = stateNumber;
             this.transition = transition;
             this.message = message;
             this.destination = destination;
+            this.receiver = receiver;
         }
 
         public void PrintState()
@@ -238,10 +355,41 @@ public class Main {
             System.out.println(transition);
             System.out.println(message);
             System.out.println(destination);
+            System.out.println(receiver);
         }
 
     }
+
+    public static class PossibleMove
+    {
+        int current;
+        int target;
+        char reciever;
+        char entity;
+
+        public PossibleMove(int current, int target, char reciever,char entity) {
+            this.current = current;
+            this.target = target;
+            this.reciever = reciever;
+            this.entity = entity;
+        }
+
+        public int getCurrent() {
+            return current;
+        }
+
+        public int getTarget() {
+            return target;
+        }
+
+        public char getReciever() {
+            return reciever;
+        }
+
+        public char getEntity() {
+            return entity;
+        }
+    }
+
+
 }
-
-
-
